8aa9b82acd298e4e7fca11f0e831b553a69bc560
Theo Benmeridja <benmeridjatheo@gmail.com>
2020-05-09 19:57:46 +0200

    trois commit

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-// for(var i=0; i<document.getElementsByClassName('text').length; i++) {
-//  console.log(document.getElementsByClassName('text')[i].length);
-//
-//
-// }
-
 document.getElementById('messageCounter').textContent = document.getElementsByClassName('text').length;
 
 for(var i=0; i<document.getElementsByClassName('trash').length; i++) {
